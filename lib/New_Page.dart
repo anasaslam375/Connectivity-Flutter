53269e8ddda5c57377d736f506b19a382fa2e1d0
Anas Aslam <91545706+anasaslam375@users.noreply.github.com>
2022-03-12 11:24:50 +0500

    Connectivity Model Done.

---
+++ lib/New_Page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class NewPage extends StatefulWidget {
+  const NewPage({ Key? key }) : super(key: key);
+
+  @override
+  _NewPageState createState() => _NewPageState();
+}
+
+class _NewPageState extends State<NewPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("New Page"),
+    );
+  }
+}
